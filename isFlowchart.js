d0ae7e8b62120e2982993fab124654c578be0154
Adriana Cottle <adriana.cottle@gmail.com>
2020-02-10 17:33:37 -0700

    adding more if statements

--- isFlowchart.js
+++ isFlowchart.js
@@ -1,15 +1,16 @@
-
 var sStudent = "";
 var fUserGpa = "";
-//var bMoveOn = false;
+var bMoveOn = false;
 var sPreReqGrade = "";
 var sProfessional = "";
 var sRejection = "You might want to apply to a different major my friend";
+var iCredits = 0;
+var sUserResponse = "";
 
 sStudent = prompt("What is your name?");
 sPreReqGrade = prompt("Did you get an A in both IS 201 and IS 303?", "Yes");
 fUserGpa = parseFloat(prompt("What is your overall GPA?"));
-sPreReqGrade
+
 //top section of flowchart
 if (fUserGpa > 3.7 && (sPreReqGrade.charAt(0)).toLocaleUpperCase() === "Y") {
     alert("you're doing great!");
@@ -19,9 +20,9 @@ if (fUserGpa > 3.7 && (sPreReqGrade.charAt(0)).toLocaleUpperCase() === "Y") {
 else if (fUserGpa > 3.5) {
     alert("it's okay");
     sPreReqGrade = prompt("Did you get a B+ or better in both IS 201 and IS 303?", "Yes")
-    if((sPreReqGrade.charAt(0)).toLocaleUpperCase() === "Y") {
+    if ((sPreReqGrade.charAt(0)).toLocaleUpperCase() === "Y") {
         bMoveOn = true;
-    }
+    } 
     else {
         alert("Keep going through the middle");
     }
@@ -30,18 +31,32 @@ else if (fUserGpa > 3.5) {
 else if (fUserGpa > 3.2) {
     alert("hang in there dude");
     //bMoveOn = false
-} 
+}
 //all is false
 else {
     alert(sRejection);
     //bMoveOn = false;
 }
+
+//moving down the line 
 console.log(bMoveOn);
-if(bMoveOn === true) {
-sProfessional = prompt("Are you professional in your interactions with others?", "Yes");
+if (bMoveOn === true) {
+    sProfessional = prompt("Are you professional in your interactions with others?", "Yes");
     if ((sProfessional.charAt(0)).toLocaleUpperCase() === "Y") {
         alert("Keep going!!");
-    }
+        iCredits = parseInt(prompt("How many credits do you have?"));
+        if (iCredits < 30) {
+            alert("Please apply next year");
+            alert("All done!");
+        } 
+        else if (iCredits < 60) {
+            sUserResponse = prompt("Are those mostly AP and language credits?", "Yes");
+            if ((sUserResponse.charAt(0)).toLocaleUpperCase() === "Y") {
+                alert("Please apply next year");
+                alert("All done!");
+            }
+        }
+    } 
     else {
         alert(sRejection);
     }
